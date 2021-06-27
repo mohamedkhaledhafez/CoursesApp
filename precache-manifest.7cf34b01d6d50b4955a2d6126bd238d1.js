5072e75ea6c58c9ff26a2e387fdbb48d7dac7c9d
mohamedkhaledhafez <mohamedkhaled97858@gmail.com>
2021-06-28 01:50:36 +0200

    Updates

---
+++ precache-manifest.7cf34b01d6d50b4955a2d6126bd238d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29bfaad93c266ed034027f127a24b270",
+    "url": "/CoursesApp/index.html"
+  },
+  {
+    "revision": "d7d6e0dc66bbe6f0e12b",
+    "url": "/CoursesApp/static/css/2.9233ae65.chunk.css"
+  },
+  {
+    "revision": "23453df3941b2483a290",
+    "url": "/CoursesApp/static/css/main.85b082fb.chunk.css"
+  },
+  {
+    "revision": "d7d6e0dc66bbe6f0e12b",
+    "url": "/CoursesApp/static/js/2.8a9cbd92.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/CoursesApp/static/js/2.8a9cbd92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23453df3941b2483a290",
+    "url": "/CoursesApp/static/js/main.99a0315a.chunk.js"
+  },
+  {
+    "revision": "994ba78492554ec07ee8",
+    "url": "/CoursesApp/static/js/runtime-main.ee64e1ac.js"
+  }
+]);
